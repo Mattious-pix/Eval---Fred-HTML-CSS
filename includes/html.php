bd38bf2e65fb89ec42a6fccc385d0fb6aeda0e13
Mattious-pix <ed.deline90@gmail.com>
2019-12-13 16:45:03 +0100

    Ajout de l'image dans assets img 13 12 2019

--- includes/html.php
+++ includes/html.php
@@ -2,7 +2,7 @@
 <html lang=fr dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Je fais du PHP</title>
+    <title>Achtung Panzer !</title>
     <link rel="stylesheet" href="./assets/css/style.css" />
   </head>
   <body>
