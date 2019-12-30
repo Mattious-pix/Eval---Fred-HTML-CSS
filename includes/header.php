44dc391f5498bd77b7d643b50a2df01149f043fe
Mattious-pix <ed.deline90@gmail.com>
2019-12-30 17:58:08 +0100

    Modification titres header

--- includes/header.php
+++ includes/header.php
@@ -3,11 +3,11 @@
   <nav>
     <ul>
       <li><a href="index.php?page=accueil" title="Page d'accueil">Accueil</a></li>
-      <li><a href="index.php?page=sondage" title="Accéder au sondage du jour">Des débuts ...</a></li>
-      <li><a href="index.php?page=sondage" title="Accéder au sondage du jour">... au triomphe !</a></li>
-      <li><a href="index.php?page=contact" title="Nous contacter">Contact</a></li>
-      <li><a href="index.php?page=inscription" title="Vous inscrire">Inscription</a></li>
-      <li><a href="index.php?page=mentions" title="Les mentions légales">Mentions légales</a></li>
+      <li><a href="index.php?page=Les débuts" title="Page des débuts">Des débuts ...</a></li>
+      <li><a href="index.php?page=Triomphes" title="Page des triomphes">... au triomphe !</a></li>
+      <li><a href="index.php?page=Contacts" title="Nous contacter">Contact</a></li>
+      <li><a href="index.php?page=Inscription" title="Vous inscrire">Inscription</a></li>
+      <li><a href="index.php?page=Mentions" title="Les mentions légales">Mentions légales</a></li>
     </ul>
   </nav>
 </header>
