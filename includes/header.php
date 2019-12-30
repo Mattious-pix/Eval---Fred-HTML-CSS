5cd3161297af582a7e054347f512757928c8a265
Mattious-pix <ed.deline90@gmail.com>
2019-12-30 17:51:56 +0100

    Ajout de la section inscprition dans header

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
       <li><a href="index.php?page=sondage" title="Accéder au sondage du jour">Des débuts ...</a></li>
       <li><a href="index.php?page=sondage" title="Accéder au sondage du jour">... au triomphe !</a></li>
       <li><a href="index.php?page=contact" title="Nous contacter">Contact</a></li>
+      <li><a href="index.php?page=inscription" title="Vous inscrire">Inscription</a></li>
       <li><a href="index.php?page=mentions" title="Les mentions légales">Mentions légales</a></li>
     </ul>
   </nav>
