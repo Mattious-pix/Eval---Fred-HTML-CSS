e793a306fe8f4e7f2e8b05f59c5a2e8a8e177b2e
Mattious-pix <ed.deline90@gmail.com>
2020-01-04 18:07:32 +0100

    Header remise à zéro

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,18 @@
 <header>
   <h1>Il était un petit Panzer I devenu Panther !</h1>
   <nav>
-    <ul>
+    <ul class="navigation">
       <li><a href="index.php?page=Accueil" title="Page d'accueil">Willkommen</a></li>
     </ul>
+      <ul id="menu-deroulant">
+        <li><a href="index.php?page=CV">Verrückte Geschichte</a>
+          <ul>
+            <li><a href="#">French CV</a></li>
+            <li><a href="#">CV espaÑol</a></li>
+          </ul>
+        </li>
+      </ul>
     <ul>
-    <ul id="menu-deroulant">
-          <li><a href="index.php?page=CV">Verrückte Geschichte</a>
-              <ul>
-                  <li><a href="#">French CV</a></li>
-                  <li><a href="#">CV espaÑol</a></li>
-              </ul>
-          </li>
       <li><a href="index.php?page=contact" title="Nous contacter">Kontact !</a></li>
       <li><a href="index.php?page=inscription" title="Vous inscriver">Join Bestellungen</a></li>
       <li><a href="index.php?page=mentions" title="Les mentions légales">Rechtliche Hinweise</a></li>
