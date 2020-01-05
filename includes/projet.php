ad98e687bb224f8652cd0b3c18fa539127996a53
Mattious-pix <ed.deline90@gmail.com>
2020-01-05 15:42:08 +0100

    réation projet.php pour tout mettre

---
+++ includes/projet.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+
+<title>Achtung Panzer !</title>
+<link rel="shortcut icon" href="./assets/img/tank1.jpg" />
+
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="css/project.css"/>
+<link rel="stylesheet" type="text/css" href="css/header.css"/>
+<link rel="stylesheet" type="text/css" href="css/footer.css"/>
+
+<!------------------------------------------------->
+<body>
+
+<header role="header">
+
+  <h1 id="titre"> Il était un petit Panzer I devenu Panther !</h1>
+
+  <nav>
+    <ul>
+      <li class="accueil"><a href="index.php?page=Accueil" title="Page d'accueil">Willkommen</a></li>
+
+      <li class="histoire"><a href="index.php?page=CV">Verrückte Geschichte</a>
+       <ul class="menu-deroulant">
+          <li><a id="frCV" href="CV français">French CV</a></li>
+          <li><a id="espCV" href="CV espagnol">CV espaÑol</a></li>
+       </ul>
+      </li>
+
+      <li class="contact"><a href="index.php?page=contact" title="Nous contacter">Kontact !</a></li>
+      <li class="inscription"><a href="index.php?page=inscription" title="Vous inscriver">Join Bestellungen</a></li>
+      <li class="mentions"><a href="index.php?page=mentions" title="Les mentions légales">Rechtliche Hinweise</a></li>
+
+    </ul>
+  </nav>
+
+</header>
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   require_once './functions/checkInput.php';
   require_once './includes/pdo.php';
 
-  require './includes/html.php';
-  require './includes/header.php';
+  require './includes/projet.php';
   require './includes/main.php';
   require './includes/footer.php';
