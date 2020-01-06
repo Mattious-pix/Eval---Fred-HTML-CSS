a1f7d87d03b93ea613284c2060f7eebf58b643b8
Mattious-pix <ed.deline90@gmail.com>
2020-01-06 11:50:37 +0100

    Remise à zéro CV

--- includes/CVespagnol.inc.php
+++ includes/CVespagnol.inc.php
@@ -1,4 +1,4 @@
-  <section>
+  <section classe="CV">
     <div>
     <h1>Mathieu Edl  <!--Ce qui est intéressant pour la photo dans la création html, c'est pour le référencment, car on peut mettre un titre à la photo--></h1>
   <img src="./assets/img/poilu.jpg" title="sldt esp"/>
@@ -6,34 +6,31 @@
       <address>
         <section class="maisonCV">En mi casa<br>
         <a href="mailto:alloallo@gmail;com.com"> contactos</a><br> <!--Le problème c'est de risquer de se faire spamer de merde-->
-        <a href="tel:+33622232425"> 06.22.23.24.25</a><br> <!--Le problème c'est que le numéro de téléphone est visible de tous quand il y a le référencement. Donc le risque de recevoir des intenspestifs venant des pays de merde-->
+        <a href="tel:+33622232425"> 06.22.23.24.25 </a><br> <!--Le problème c'est que le numéro de téléphone est visible de tous quand il y a le référencement. Donc le risque de recevoir des intenspestifs venant des pays de merde-->
       </address>
-        <h1 id="domaine">Dominio</h1>
+        <h2 class="domaine">Dominio</h2>
         </header>
-      <h1 id="exp">experiencias profesionales</h1> <!--Même si on met plusieurs H1, ca ne change rien à la formation de la page. Et en plus ca permet de refnorcer le référencement-->
+      <h2 class="exp">Experiencias Profesionales</h2> <!--Même si on met plusieurs H1, ca ne change rien à la formation de la page. Et en plus ca permet de renforcer le référencement. Mais en fait j'ai décidé de passer par du H2 comme il faut plutôt un seul H1 par page-->
       <div class=marge1>
-      <ul>Référant Digital – N-Factory</ul>
-        <li>09/2019-04/2020 – Stagiaire - Rouen- FRANCE</li>
+      <ul>
+        <li>Référant Digital – N-Factory</li>
+        - 09/2019-04/2020 - Rouen - FRANCE
+        <li>Master Histoire blablabla</li>
+          - 2017-2018 Paris - FRANCE
+        <li>Master blablabla</li>
+          - 2016-2017 Rouen - FRANCE
+        <li>Master blablabla</li>
+          - 2014-2016 Rouen - FRANCE
+        </ul>
+      </div>
+      <h2 class="prcr">Enseñanza</h2>
       <div class=marge2>
-        <ul>Master Histoire blablabla</ul>
-          <li>2017-2018 Paris - FRANCE</li>
+        <ul>
+        <li> Fue el primero chico que estaba en un restaurante desnudo en Paris </li>
+        <li>Hace doscientos años fue une grande Rey de Francia</li>
+      </ul>
       </div>
-      <div class=marge3>
-        <ul> Master blablabla</ul>
-          <li>2016-2017 Rouen - FRANCE</li>
-      </div>
-      <div class=marge4>
-        <ul>Master blablabla</ul>
-          <li>2014-2016 Rouen - FRANCE</li>
-      </div>
-      <h1 id="prcr">Enseñanza</h1>
-      <div class=marge5>
-        <ul> Fue el primero chico que estaba en un restaurante desnudo en Paris </ul>
-      </div>
-      <div class=marge6>
-        <ul>Hace doscientos años fue une grande Rey de Francia</ul>
-      </div>
-      <h1>Langues</h1>
+      <h2 class="lng"> Langues</h2>
         <ul>
           <li>Frances : Mi vida !</li>
           <li>Anglais : Para los bajos </li>
--- includes/CVfrançais.inc.php
+++ includes/CVfrançais.inc.php
@@ -1,46 +1,44 @@
-  <section>
+  <section class="CV">
     <div>
-    <h1>Mathieu Edl  <!--Ce qui est intéressant pour la photo dans la création html, c'est pour le référencment, car on peut mettre un titre à la photo--></h1>
-  <img src="./assets/img/PanzerIIausfB.jpg" title="char cv fr"/>
+      <h1>Mathieu Edl  <!--Ce qui est intéressant pour la photo dans la création html, c'est pour le référencment, car on peut mettre un titre à la photo--></h1>
+        <img src="./assets/img/PanzerIIausfB.jpg" title="char cv fr"/>
     </div>
       <address>
-        <section class="maisonCV">chez oim<br/>
+          <section class="maisonCV">chez oim<br/>
         <a href="mailto:alloallo@gmail;com.com"> contact</a></br> <!--Le problème c'est de risquer de se faire spamer de merde-->
-        <a href="tel:+33622232425"> 06.22.23.24.25</a></br> <!--Le problème c'est que le numéro de téléphone est visible de tous quand il y a le référencement. Donc le risque de recevoir des intenspestifs venant des pays de merde-->
+        <a href="tel:+33622232425"> 06.22.23.24.25 </a></br> <!--Le problème c'est que le numéro de téléphone est visible de tous quand il y a le référencement. Donc le risque de recevoir des intenspestifs venant des pays de merde-->
       </address>
-        <h1 id="domaine">Poste-domaine</h1>
+        <h2 class="domaine">Poste-domaine</h2>
         </header>
-      <h1 id="exp">Ausbildung</h1> <!--Même si on met plusieurs H1, ca ne change rien à la formation de la page. Et en plus ca permet de refnorcer le référencement-->
+      <h2 class="exp">Ausbildung</h2> <!--Même si on met plusieurs H1, ca ne change rien à la formation de la page. Et en plus ca permet de refnorcer le référencement-->
       <div class=marge1>
-      <ul>Référant Digital – N-Factory</ul>
-        <li>09/2019-04/2020 – Stagiaire - Rouen- FRANCE</li>
+      <ul>
+        <li>Référant Digital – N-Factory</li>
+        - 09/2019-04/2020 - Rouen- FRANCE
+        <li>Master Histoire blablabla</li>
+          - 2017-2018 Paris - FRANCE
+        <li>Master blablabla</li>
+          - 2016-2017 Rouen - FRANCE
+        <li>Master blablabla</li>
+          - 2014-2016 Rouen - FRANCE
+        </ul>
+      </div>
+      <h2 class="prcr">Kurs</h2>
       <div class=marge2>
-        <ul>Master Histoire blablabla</ul>
-          <li>2017-2018 Paris - FRANCE</li>
+        <ul>
+        <li>Grand gagnant de la belotte de Winchester. </li>
+        <li>Découvreur du remède contre les ongles incarnés</li>
+        <li>Partenaire de Jo le trembleur dans la concoction de la limonade</li>
+      </ul>
       </div>
-      <div class=marge3>
-        <ul> Master blablabla</ul>
-          <li>2016-2017 Rouen - FRANCE</li>
-      </div>
-      <div class=marge4>
-        <ul>Master blablabla</ul>
-          <li>2014-2016 Rouen - FRANCE</li>
-      </div>
-      <h1 id="prcr">Kurs</h1>
-      <div class=marge5>
-        <ul> Grand gagnant de la belotte de Winchester. </ul>
-      </div>
-      <div class=marge6>
-        <ul>Découvreur du remède contre les ongles incarnés</ul>
-      </div>
-      <h1>Langues</h1>
+      <h2>Langues</h2>
         <ul>
           <li>Français : maternelle</li>
           <li>Anglais : Confirmé </li>
           <li>Espagnol: Intermédiaire</li>
           <li>Grec : Notion.</li>
-          </ul>
+        </ul>
       </div>
     </body>
   </html>
-</section>
+  </section>
