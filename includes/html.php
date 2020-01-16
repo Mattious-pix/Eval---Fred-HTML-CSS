798ec564aa5349cff363b9281910ec10c9ebfb17
Mattious-pix <ed.deline90@gmail.com>
2020-01-16 17:00:42 +0100

    Commiter essais ratés

--- includes/html.php
+++ includes/html.php
@@ -8,7 +8,8 @@
 
   <title>Achtung Panzer !</title>
   <link rel="shortcut icon" href="./assets/img/tank1.jpg" />
-
+  <link src=".assets/js/jquery-ui.js">
+  <link src=".assets/js/jquery.js">
   <link rel="stylesheet" href="./assets/css/style.css"/>
 
 </head>
